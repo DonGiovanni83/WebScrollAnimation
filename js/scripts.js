3b7cfe92ef6f2ac63216c036f963714af7177bd7
Fabio Bertagna <flbertagna@gmail.com>
2019-01-06 17:39:39 +0100

    Added navbar and content placeholder

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,27 @@ var frameNumber = 0, // start video at frame 0
     // get page height from video duration
     setHeight = document.getElementById("set-height"), 
     // select video element         
-    vid = document.getElementById('v0'); 
+    vid = document.getElementById('v0'), 
     // var vid = $('#v0')[0]; // jquery option
+    navbar = document.getElementById('navbar');
 
 vid.addEventListener('loadedmetadata', function() {
   setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
 });
+
+window.addEventListener('scroll' ,() =>  {
+    var yOffset = window.pageYOffset;
+    var height = document.body.clientHeight/3;
+    console.log(yOffset);
+    if (yOffset < height){
+        navbar.style.opacity = yOffset/height;
+        console.log("opacity : " + yOffset);
+    }
+    else if (yOffset >= height){
+        navbar.style.opacity = 1;
+    }
+});
+
 
 
 // Use requestAnimationFrame for smooth playback
@@ -20,3 +35,6 @@ function scrollPlay(){
 }
 
 window.requestAnimationFrame(scrollPlay);
+
+
+
